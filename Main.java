c77d3e156a5c7f86531a19881811532dffbfd9ad
Merve <mervetutoglu817@gmail.com>
2021-11-22 18:31:31 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {7, 9, 11, 2};
+	    int N = numbers.length;
+	    boolean changed = true;
+	    while (changed)
+        {
+            changed = false;
+            for (int i = 0; i <= N -2; i++)
+            {
+                if (numbers [i] > numbers [i + 1])
+                {
+                    swap (numbers, i, i + 1);
+                    changed = true;
+                }
+            }
+        }
+	    for (int n : numbers)
+            System.out.print(n + " ");
+    }
+    public static void swap (int [] array, int ind1, int ind2){
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+
+}
